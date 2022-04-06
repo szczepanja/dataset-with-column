d7a9cf4242aac173134de2ee52ce62fea4a3bfbe
a_szczepanik <a.szczepanik@adform.com>
2022-04-06 10:10:34 +0200

    Name to uppercase

---
+++ src/main/scala/Web.scala
@@ -0,0 +1,23 @@
+
+object Web extends App {
+
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL example")
+    .master("local[*]")
+    .getOrCreate()
+
+  val people = spark
+    .read
+    .option("header", value = true)
+    .option("inferSchema", value = true)
+    .csv("data.csv")
+
+  import org.apache.spark.sql.functions.upper
+
+  val upperColumn = upper(people("name"))
+  val peopleUpper = people.withColumn("UPPER", upperColumn)
+  peopleUpper.show()
+}
